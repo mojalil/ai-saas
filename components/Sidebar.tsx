49b13d68199cac70c8853b2c610c312fd1ded512
motypes <mo@openfren.com>
2023-07-28 16:08:10 +0800

    added sidebar for mobile

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { LayoutDashboard } from "lucide-react";
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon } from "lucide-react";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -17,7 +17,43 @@ const routes = [
         icon: LayoutDashboard,
         href: "/dashboard",
         color: "text-sky-500",
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-700",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-500",
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        href: "/music",
+        color: "text-emerald-500",
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700",
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
     }
+
 ]
 
 const Sidebar = () => {
