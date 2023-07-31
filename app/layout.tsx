c490a6b3bb69cf87a2d14c0fe04c9b4024b8f0d8
motypes <mo@openfren.com>
2023-07-31 09:43:43 +0800

    initial version of the conversations route is complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Genius AI Platform',
+  title: 'AI SaaS',
   description: 'AI platform for music, video, text and more...',
 }
 
---
+++ components/BotAvatar.tsx
@@ -0,0 +1,13 @@
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+const BotAvatar = () => {
+
+    return(
+        <Avatar className="h-8 w-8">
+            <AvatarImage src="ai-saas-logo-5.png" alt="avatar" />
+        </Avatar>
+    )
+}
+
+export default BotAvatar;
